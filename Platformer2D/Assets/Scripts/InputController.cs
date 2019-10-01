726476314879c265a9f17ddc83814624cbc91622
Lucas Maillet <lucas_maillet@telecom-sudparis.eu>
2019-10-01 14:54:25 +0200

    J'ai fait un 1put controller

---
+++ Platformer2D/Assets/Scripts/InputController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private float speed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            Debug.Log("JUMP !!  JUMP !!  JUMP!!");
+        }
+        else if (Input.GetButton("Dash"))
+        {
+            Debug.Log("Dashi Dashi !!!");
+        }
+
+        Debug.Log(Input.GetAxis("Horizontal") + "  Taille Quentin : " + Input.GetAxis("Vertical"));
+    }
+}
