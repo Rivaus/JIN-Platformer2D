da56143c9aea25cf4e3cfed8d62eb956e689bd0d
Lucas Maillet <lucas_maillet@telecom-sudparis.eu>
2019-10-01 15:43:56 +0200

    le saut nous emmene tres haut trestres haut

---
+++ Platformer2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    BoxCollider boxCollider;
+    Vector2 velocity;
+
+    private float speed;
+    private float acceleration;
+    private bool isGrounded = true;
+
+    [SerializeField]
+    float gravity;
+    [SerializeField]
+    float walkSpeed;
+    [SerializeField]
+    float walkAcceleration;
+
+    [SerializeField]
+    float runSpeed;
+    [SerializeField]
+    float runAcceleration;
+
+    [SerializeField]
+    float jumpForce;
+
+
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+        Debug.Assert(boxCollider != null, "Le joueur doit posséder une box de collision");
+    }
+
+    private void Update()
+    {
+        ComputeMovment();
+        transform.Translate(velocity * Time.deltaTime);
+    }
+
+    void ComputeMovment()
+    {
+        if(Input.GetAxisRaw("Run") > 0)
+        {
+            speed = runSpeed;
+            acceleration = runAcceleration;
+        }
+        else
+        {
+            speed = walkSpeed;
+            acceleration = walkAcceleration;
+        }
+        
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInput, acceleration * Time.deltaTime);
+
+        if (isGrounded && Input.GetButtonDown("Jump") ){
+            Jump();
+        }
+        velocity.y += -gravity*Time.deltaTime;
+    }
+
+    //precondition le joueur a appuyé sur Jump et isGrounded
+    void Jump()
+    {
+        velocity.y = jumpForce;
+    }
+}
+
